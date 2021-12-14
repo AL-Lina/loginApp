e0a8cf2d3eb809fd6ee6a170dbeddeca4bd14dd6
AL-Lina <lina-sokolova00@mail.ru>
2021-12-15 02:51:20 +0300

    Create gradient color for WelcomeViewController

--- loginApp/WelcomeViewController.swift
+++ loginApp/WelcomeViewController.swift
@@ -12,6 +12,14 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.type = .axial
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
     }
     
     @IBAction func closeScreen() {
